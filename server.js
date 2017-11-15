277e8d96ef92a2347578ecbafe22a0ab15b07ac2
Giulio Pietroiusti <giulio.pietroiusti@gmail.com>
2017-11-15 18:07:38 +0100

    Avoid favicon request

--- server.js
+++ server.js
@@ -4,14 +4,24 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', function(req, res){
-    res.json(
-	{
-	    "ipaddress": null,
-	    "language": null,
-	    "sofware": null
-	}
-    );
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+app.get('/:parameter', function(req, res){
+    // avoid favicon request: https://stackoverflow.com/questions/17952436/node-js-double-console-log-output
+    if(req.url === '/favicon.ico')
+    {
+	console.log('favicon requested');
+    } else {
+
+	res.json(
+	    {
+		"ipaddress": null,
+		"language": null,
+		"sofware": null
+	    }
+	);
+    }
 });
 
 app.listen(process.env.PORT || 3000);
